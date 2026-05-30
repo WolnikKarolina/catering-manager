82ec56bebcb51d9782a2b47b1b53906851c0f70c
Karola <wolnikkarolina1@gmail.com>
2026-05-30 13:15:15 +0200

    add DatabaseConnection class

---
+++ src/main/java/pl/karolina/cateringmanager/db/DatabaseConnection.java
@@ -0,0 +1,16 @@
+package pl.karolina.cateringmanager.db;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/catering_manager";
+    private static final String USER = "root";
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER,PASSWORD);
+    }
+
+}
