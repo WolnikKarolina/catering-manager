dcec7f875fe2efc3513519d5f137289ec41c336e
Karola <wolnikkarolina1@gmail.com>
2026-06-02 21:45:21 +0200

    update OrderRepository

--- src/main/java/pl/karolina/cateringmanager/repository/OrderRepository.java
+++ src/main/java/pl/karolina/cateringmanager/repository/OrderRepository.java
@@ -2,7 +2,6 @@ package pl.karolina.cateringmanager.repository;
 
 import pl.karolina.cateringmanager.db.DatabaseConnection;
 import pl.karolina.cateringmanager.model.Calories;
-import pl.karolina.cateringmanager.model.Client;
 import pl.karolina.cateringmanager.model.DietType;
 import pl.karolina.cateringmanager.model.Order;
 
@@ -10,7 +9,6 @@ import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class OrderRepository {
 
@@ -30,12 +28,62 @@ public class OrderRepository {
         }
     }
 
-    public List<Order> findByClientId(int id) {
+    public Order findOrderById(int orderId) {
+        Order order = new Order();
+       try(Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(
+                    "SELECT * FROM orders WHERE id = ?")) {
+            stmt.setInt(1, orderId);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                order.setId(rs.getInt("id"));
+                order.setClientId(rs.getInt("client_id"));
+                order.setDate(rs.getDate("date").toLocalDate());
+                order.setCalories(Calories.fromKcal(rs.getInt("calories")));
+                order.setDietType(DietType.valueOf(rs.getString("diet_type")));
+                order.setDiscount(rs.getDouble("discount"));
+                order.setPrice(rs.getDouble("price"));
+                return order;
+            }
+            throw new RuntimeException("Order not found: " + orderId);
+       } catch (SQLException e) {
+           throw new RuntimeException("Failed to get order with id: " + orderId, e);
+       }
+    }
+
+    public List<Order> findOrdersByClientIdByDates(int clientId, LocalDate startDate, LocalDate finishDate) {
+        List<Order> orders = new ArrayList<>();
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(
+                     "SELECT * FROM orders WHERE client_id = ? AND date BETWEEN ? AND ?")) {
+            stmt.setInt(1, clientId);
+            stmt.setDate(2, Date.valueOf(startDate));
+            stmt.setDate(3, Date.valueOf(finishDate));
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    Order order = new Order();
+                    order.setId(rs.getInt("id"));
+                    order.setClientId(rs.getInt("client_id"));
+                    order.setDate(rs.getDate("date").toLocalDate());
+                    order.setCalories(Calories.fromKcal(rs.getInt("calories")));
+                    order.setDietType(DietType.valueOf(rs.getString("diet_type")));
+                    order.setDiscount(rs.getDouble("discount"));
+                    order.setPrice(rs.getDouble("price"));
+                    orders.add(order);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to fetch orders for client id: " + clientId, e);
+        }
+        return orders;
+    }
+
+    public List<Order> findOrdersByClientId(int clientId) {
         List<Order> orders = new ArrayList<>();
         try (Connection conn = DatabaseConnection.getConnection();
              PreparedStatement stmt = conn.prepareStatement(
                      "SELECT * FROM orders WHERE client_id = ?")) {
-            stmt.setInt(1, id);
+            stmt.setInt(1, clientId);
             try (ResultSet rs = stmt.executeQuery()) {
                 while (rs.next()) {
                     Order order = new Order();
@@ -49,7 +97,7 @@ public class OrderRepository {
                 }
             }
         } catch (SQLException e) {
-            throw new RuntimeException("Failed to fetch orders for client id: " + id, e);
+            throw new RuntimeException("Failed to fetch orders for client id: " + clientId, e);
         }
         return orders;
     }
@@ -71,14 +119,27 @@ public class OrderRepository {
         }
     }
 
-    public void delete(int id) {
+    public void deleteOneOrder(int orderId) {
         try(Connection conn = DatabaseConnection.getConnection();
         PreparedStatement stmt = conn.prepareStatement(
-                "DELETE  FROM orders WHERE id = ?")){
-            stmt.setInt(1, id);
+                "DELETE FROM orders WHERE id = ?")){
+            stmt.setInt(1, orderId);
             stmt.executeUpdate();
         } catch (SQLException e) {
-            throw new RuntimeException("Failed to delete order with id: " + id, e);
+            throw new RuntimeException("Failed to delete order with id: " + orderId, e);
+        }
+    }
+
+    public void deleteOrdersByDates (int clientId, LocalDate startDate, LocalDate finishDate) {
+        try(Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(
+                    "DELETE FROM orders WHERE id = ? AND date BETWEEN ? AND ?")){
+            stmt.setInt(1, clientId);
+            stmt.setDate(2, Date.valueOf(startDate));
+            stmt.setDate(3, Date.valueOf(finishDate));
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to delete orders", e);
         }
     }
 
